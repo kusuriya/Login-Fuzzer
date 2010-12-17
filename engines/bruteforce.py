b0962b7f25af3265d716c0d54fa26e4059ee846d
Kyle Isom <coder@kyleisom.net>
2010-12-17 15:55:49 -0700

    Update README. Begin overhaul of Bruteforcer.
    README has been updated to reflect more information on using the tools
    in the project, as well as usage cases / non-cases.
    
    Bruteforcer has been cleared out and will be a derived class from Fuzzer.

--- engines/bruteforce.py
+++ engines/bruteforce.py
@@ -2,49 +2,12 @@
 
 import datetime
 import sys
-from ..interfaces import ssh
+from fuzzer import Fuzzer
 
 
-def Bruteforcer( ):
+class Bruteforcer(Fuzzer):
     """
-    Brute force engine.
+    Brute force engine. Extension of fuzzer.
     """
-    alphabet        = None
-    host            = None
-    user            = None
-    interface       = None
-    target          = None
+    pass
     
-    err             = sys.stderr.write
-    
-    
-    def set_alphabet(self, alphabet_str):
-        self.alphabet     = [ ]
-        self.alphabet_set = set(alphabet_str).intersection(set(alphabet_str))
-        for i in range(len(alphabet_set)):
-            self.alphabet.append(ord(alphabet_set.pop()))
-        self.alphabet     = sorted(self.alphabet)
-    
-    def generate_passwords(self, start = 1, end = 16):
-        for width in range(start, end + 1):
-            self._recurse(width, 0, "")
-    
-    def _recurse(self, width, position, base_str):
-        #current position
-        for char in alphabet:
-            if (position < width - 1):
-                test_pass[position] = char
-                self._recurse(width, position + 1, test_pass)
-            if not test_password(host, user, ''.join(test_pass)):
-                if not exception_type == paramiko.AuthenticationException:
-                    err('ssh error!')
-                    sys.exit(2)
-    
-    def crack(self, start, end, alphabet_str = None):
-        if alphabet_str:
-            set_alphabet(alphabet_str)
-        
-        self.generate_passwords(start, end)
-
-    def set_target(self, host, user):
-        self.target  = ssh()
