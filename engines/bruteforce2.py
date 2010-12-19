499f58a221b7c1f2dabdfb9065a1b34cf71d51d8
Kyle Isom <coder@kyleisom.net>
2010-12-18 18:39:26 -0700

    bruteforce2 now in repo, fuzzer2mp started.
    bruteforce2 apparently wasn't added in the last commit. This has been
    fixed.
    
    Copied fuzzer2 over to fuzzer2mp to begin making the code reentrant and
    thread safe.

---
+++ engines/bruteforce2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+"""
+bruteforce engine revamped with new fuzzer2 backend.
+"""
+
+import datetime
+import sys
+from fuzzer2 import Fuzzer
+
+UPPER   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER   = 'abcdefghijklmnopqrstuvwxyz'
+NUM     = '0123456789'
+SPECIAL = '`~@#$%^&*()_+=[]{}\|\'"/><'
+PUNC    = '.?!,;:'
+
+class Bruteforcer(Fuzzer):
+    """
+    Brute force engine. Extension of fuzzer.
+    
+    Constructor parameters:
+        start   - start with n characters
+        end     - end with n characters
+        target  - interface target
+        upper   - use uppercase characters
+        lower   - use lowercase characters
+        num     - use numbers
+        special - use special characters
+        punc    - use punctuation characters
+    """
+    
+    def __init__(self, start, end, target, upper = False, lower = False, 
+                 num = False, special = False, punc = False):
+        self.start  = start
+        self.end    = end
+        self.target = target
+        self.fuzz_set   = [ ]
+    
+        for i in range(end):
+            charset = ""
+            if upper:   charset += UPPER
+            if lower:   charset += LOWER
+            if num:     charset += NUM
+            if special: charset += SPECIAL
+            if punc:    charset += PUNC
+        
+            self.fuzz_set.append(charset)
+
+    def set_target(self, target):
+        self.target = target
+
+def selector( ):
+    upper       = False
+    lower       = False
+    num         = False
+    special     = False
+    punc        = False
+    
+    upper   = 'y' == raw_input('Use uppercase characters (y/n)? ').lower()[0]
+    lower   = 'y' == raw_input('Use lowercase characters (y/n)? ').lower()[0]
+    num     = 'y' == raw_input('Use numbers (y/n)? ').lower()[0]
+    special = 'y' == raw_input('Use special characters (y/n)? ').lower()[0]
+    punc    = 'y' == raw_input('Use punctuation (y/n)? ').lower()[0]
+    
+    start   = raw_input('Start with how many characters? ')
+    if not start.isdigit():
+        print 'expected a numeric value...'
+        return None
+    else:
+        start   = int(start)
+    
+    end     = raw_input('End at how many characters? ')
+    if not end.isdigit():
+        print 'expected a numeric value...'
+        return None
+    elif int(end) < start:
+        print 'end should be greater than or equal to start!'
+        return None
+    else:
+        end     = int(end)
+    
+    return Bruteforcer(start, end, None, upper, lower, num, special, punc)
---
+++ engines/fuzzer2mp.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+New password generation backend, suggested by Evan.
+"""
+
+import itertools
+
+class Fuzzer():
+    fuzz_set       = None
+    start          = None
+    end            = None
+    target         = None
+    password       = None
+    
+    def __init__(self, i_fuzz_set, start, end, target):
+        self.fuzz_set = i_fuzz_set
+        self.start = start
+        self.end = end
+        self.target = target 
+
+    def crack(self):
+        for i in range(self.start, self.end + 1):
+            fuzz_set = self.fuzz_set[:i + 1]
+
+            for j in itertools.product(*fuzz_set):
+                password = ''.join(j)
+                if self.target.trypass(password):
+                    print "MATCH:", password
+                    return password
+
+    def set_target(self, target):
+
+        self.target = target
+
+def selector( ):
+    in_str      = "NOT_NULL"
+    fuzz_set    = [ ]
+    i           = 0
+    
+    print "Enter characters for fuzz_set[%d] as a string sequence, empty " % i
+    print "line to end."    
+    while in_str:
+        in_str = raw_input('\tstring for position' + str(i) + ': ')
+        
+        if in_str:
+            fuzz_set.append(in_str)
+            i += 1
+        
+        
+    start       = raw_input('minimum number of chars:')
+    end         = raw_input('maximum number of chars:')
+
+    if not start.isdigit() or not end.isdigit():
+        print 'Number of characters must be a numberic (integer) value!'
+        return None
+    else:
+        start   = int(start)
+        end     = int(end)
+        return Fuzzer(fuzz_set, start, end, None)
